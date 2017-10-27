f90f2f0e79e6c3e2b12112904381c2a38577d2d8
DESKTOP-2IO1N0U\Stefanos <stefkaps@utexas.edu>
2017-10-27 16:07:31 -0500

    Insight 4 Images, and 2nd run of QDA, LDA, and Linear Regression code

--- Project3_SKapetanakis.R
+++ Project3_SKapetanakis.R
@@ -21,70 +21,18 @@ summary(df)
 #Add binary column for gender
 df <- df %>% dplyr::mutate(sex2 = ifelse(sex == "true", 1, 0)) 
 
-#Add column for age <=65 or not
-df <- df %>% dplyr::mutate(age2 = ifelse(age <= 65, 1, 0)) #so 1 means age<=65 and 0 means age>65
-summary(df)
-
 attach(df)
 
 
-### Insight 3 - age2 ###
+#### Insight 1 - Logistic Regression, LDA, & QDA Predicting sex2 with Marcus's wrong KNN### 
 
-##Best Subset Section##
-
-# Builds a new dataframe that excludes a column that should not be included as a predictor.
-df_subset <- df %>% dplyr::select(., -subject, -age, -sex2)
-
-regfit.full=regsubsets(age2~.,data=df_subset, nvmax=22)
-reg.summary=summary(regfit.full)
-names(reg.summary)
-plot(reg.summary$cp,xlab="Number of Variables",ylab="Cp")
-which.min(reg.summary$cp)
-#points(10,reg.summary$cp[10],pch=20,col="red")
-
-plot(reg.summary$adjr2,xlab="Number of Variables",ylab="adjr2")
-which.min(reg.summary$adjr2)
-
-summary(regfit.full)
-
-plot(regfit.full,scale="Cp")
-plot(regfit.full,scale="adjr2")
-coef(regfit.full,10)
-
-##Forward Selection Section##
-
-regfit.fwd=regsubsets(age2~.,data=df_subset,nvmax=22,method="forward")
-summary(regfit.fwd)
-
-plot(regfit.fwd,scale="Cp")
-plot(regfit.fwd,scale="adjr2")
-
-regfwd.summary=summary(regfit.fwd)
-
-which.min(regfwd.summary$cp)
-which.min(regfwd.summary$adjr2)
-
-plot(regfwd.summary$cp,xlab="Number of Variables",ylab="Cp")
-plot(regfwd.summary$adjr2,xlab="Number of Variables",ylab="adjr2")
-
-#KNN 
-predictorsKNN2=cbind(age, hnr, dfa)
-knn2.pred=class::knn(predictorsKNN2[train, ],predictorsKNN2[test_knn,],total_updrs[train],k=1)
-table(knn2.pred,total_updrs[test_knn])
-mean(knn2.pred==total_updrs[test_knn])
-
-
-
-
-### KNN ### (marcus)
-predictorsKNN=cbind(age, motor_updrs, total_updrs, jitter, jitter_abs, jitter_ppq5, rpde, dfa, ppe)
+# Marcus' Wrong KNN
+predictorsKNN7=cbind(age, motor_updrs, total_updrs, jitter, jitter_abs, jitter_ppq5, rpde, dfa, ppe)
 knn7.pred=class::knn(predictorsKNN7[train, ],predictorsKNN7[test_knn,],sex2[train],k=1)
 table(knn7.pred,sex2[test_knn])
 mean(knn7.pred==sex2[test_knn])
 
-
-
-### Logistic Regression ###
+# Logistic Regression
 #1 age+rpde+ppe+total_updrs predicting sex2
 set.seed(1)
 train = sample(nrow(df), 2937)
@@ -101,8 +49,7 @@ sex2.test = test$sex2
 table(glm1.pred,sex2.test) #confusion matrix
 mean(glm1.pred==sex2.test) #bad mean
 
-
-### LDA ###
+# LDA
 #1 age+rpde+ppe+total_updrs predicting sex2
 lda1.fit=lda(sex2 ~ age + rpde + ppe + total_updrs,
              data=df, subset=train)
@@ -112,8 +59,7 @@ lda1_df = data.frame(lda1.pred)
 table(lda1.pred$class,test$sex2) #confusion matrix
 mean(lda1.pred$class==test$sex2) #bad mean
 
-
-### QDA ###
+# QDA
 #1 age+rpde+ppe+total_updrs predicting sex2
 qda1.fit = qda(sex2 ~ age + rpde + ppe + total_updrs,
                data=df, subset=train)
@@ -123,14 +69,16 @@ table(qda1.pred$class,test$sex2)
 mean(qda1.pred$class==test$sex2)
 
 
-### KNN ###
+# KNN
 predictorsKNN=cbind(age, motor_updrs, total_updrs, jitter, jitter_abs, jitter_ppq5, rpde, dfa, ppe)
 knn7.pred=class::knn(predictorsKNN7[train, ],predictorsKNN7[test_knn,],sex2[train],k=1)
 table(knn7.pred,sex2[test_knn])
 mean(knn7.pred==sex2[test_knn])
 
 
-### Insight 2: hnr vs Shimmer ###
+
+### Insight 2: hnr vs Shimmer - LOOCV & K-Fold CV ###
+
 plot(hnr~shimmer, data = df)
 #ggplot(df, aes(x=shimmer, y=hnr)) + geom_hex(bins = 50)
 glm.fit=glm(hnr~shimmer, data=df)
@@ -158,3 +106,109 @@ for(d in degree){
   cv.error10[d]=cv.glm(df,glm.fit,K=10)$delta[1]
 }
 lines(degree,cv.error10,type="b",col="red")
+
+
+
+### Insight 3 - Best Subset Section predicting age2 ###
+
+# Builds a new dataframe that excludes a column that should not be included as a predictor.
+df_subset <- df %>% dplyr::select(., -subject, -age, -sex2)
+
+regfit.full=regsubsets(age2~.,data=df_subset, nvmax=22)
+reg.summary=summary(regfit.full)
+names(reg.summary)
+plot(reg.summary$cp,xlab="Number of Variables",ylab="Cp")
+which.min(reg.summary$cp)
+#points(10,reg.summary$cp[10],pch=20,col="red")
+
+plot(reg.summary$adjr2,xlab="Number of Variables",ylab="adjr2")
+which.min(reg.summary$adjr2)
+
+summary(regfit.full)
+
+plot(regfit.full,scale="Cp")
+plot(regfit.full,scale="adjr2")
+coef(regfit.full,10)
+
+
+
+### Insight 4 - Forward Selection predicting age2###
+
+#Add column for age <=65 or not
+df <- df %>% dplyr::mutate(age2 = ifelse(age <= 65, 1, 0)) #so 1 means age<=65 and 0 means age>65
+#summary(df)
+
+# Builds a new dataframe that excludes a column that should not be included as a predictor.
+df_subset <- df %>% dplyr::select(., -subject, -age, -sex2)
+
+regfit.fwd=regsubsets(age2~.,data=df_subset,nvmax=22,method="forward")
+summary(regfit.fwd)
+
+plot(regfit.fwd,scale="Cp")
+plot(regfit.fwd,scale="adjr2")
+
+regfwd.summary=summary(regfit.fwd)
+
+which.min(regfwd.summary$cp)
+which.min(regfwd.summary$adjr2)
+
+plot(regfwd.summary$cp,xlab="Number of Variables",ylab="Cp")
+plot(regfwd.summary$adjr2,xlab="Number of Variables",ylab="adjr2")
+
+
+
+#### Insight 5 - Logistic Regression, LDA, & QDA Predicting sex2 with Marcus's CORRECT KNN### 
+
+#Same Best Subset Code with FIXED Dataframe#
+df_subset_fixed <- df %>% dplyr::select(., -subject, -sex)
+#head(df_subset_fixed)
+
+# Marcus' corrected KNN
+set.seed(1)
+train = sample(nrow(df), 2937)
+test = df[-train,]
+
+test_knn = sample(nrow(test), 2937)
+predictorsKNN8=cbind(motor_updrs, total_updrs, jitter_abs, jitter_rap, jitter_ppq5, shimmer_apq11, nhr, hnr, rpde, ppe)
+knn8.pred=class::knn(predictorsKNN8[train, ],predictorsKNN8[test_knn,],sex2[train],k=1)
+table(knn8.pred,sex2[test_knn])
+mean(knn8.pred==sex2[test_knn])
+
+# Logistic Regression
+set.seed(1)
+train = sample(nrow(df_subset_fixed), 2937)
+test = df_subset_fixed[-train,]
+
+glm2.fit=glm(sex2 ~ motor_updrs + total_updrs + jitter_abs + jitter_rap + jitter_ppq5 + shimmer_apq11 + nhr + hnr + rpde + ppe,
+             data=df, family=binomial,
+             subset=train)
+summary(glm2.fit)
+
+glm2.probs=predict(glm2.fit,newdata=test,type="response")
+glm2.pred=ifelse(glm2.probs>0.5,"1","0")
+sex2.test = test$sex2
+table(glm2.pred,sex2.test) #confusion matrix
+mean(glm2.pred==sex2.test) #bad mean
+
+
+#Pairs Chart#
+pairs_df <- df %>% dplyr::select(., motor_updrs, total_updrs, jitter_abs, jitter_rap, jitter_ppq5, shimmer_apq11, nhr, hnr, rpde, ppe)
+
+pairs(pairs_df)
+
+# LDA
+lda2.fit=lda(sex2 ~ motor_updrs + total_updrs + jitter_rap + jitter_ppq5 + shimmer_apq11 + nhr + hnr + rpde + ppe,
+             data=df, subset=train)
+lda2.fit
+lda2.pred=predict(lda2.fit, test)
+lda2_df = data.frame(lda2.pred)
+table(lda2.pred$class,test$sex2) #confusion matrix
+mean(lda2.pred$class==test$sex2) #bad mean
+
+# QDA
+qda2.fit = qda(sex2 ~ motor_updrs + total_updrs + jitter_abs + jitter_rap + jitter_ppq5 + shimmer_apq11 + nhr + hnr + rpde + ppe,
+               data=df, subset=train)
+qda2.fit
+qda2.pred = predict(qda2.fit, test)
+table(qda2.pred$class,test$sex2)
+mean(qda2.pred$class==test$sex2)
